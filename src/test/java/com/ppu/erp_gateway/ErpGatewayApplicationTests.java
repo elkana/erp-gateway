65cafc0beac6127864f7a8f9632e539ad83b6251
elkana <elkana911@yahoo.com>
2024-07-30 08:50:09 +0700

    first

---
+++ src/test/java/com/ppu/erp_gateway/ErpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppu.erp_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
